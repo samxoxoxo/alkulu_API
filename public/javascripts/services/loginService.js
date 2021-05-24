6302ba7cf5b7087d1996d5cf32efedd25a5e7be5
samxoxoxo <rahibsam2@gmail.com>
2021-05-24 20:34:45 +0530

    g minor

--- public/javascripts/services/loginService.js
+++ public/javascripts/services/loginService.js
@@ -1,5 +1,5 @@
 var UserSchema = require('../models/UserSchema')
-var bcrypt = require('bcrypt');
+var bcrypt = require('bcryptjs');
 var saltRouds = 10
 module.exports = {
     login: async function(email, password) {
